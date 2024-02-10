efafe03cb68a66335841c2d51dbd69237d6f95c7
shazyaaa <shazyaaudrea@gmail.com>
2024-02-10 23:46:02 +0700

    modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -1,38 +1,40 @@
 import random
 import time
+import os
+
 def read_file(file_name):
     with open(file_name, 'r') as file:
         lines = file.readlines()
         buffer_size = int(lines[0])
         matrix_size = tuple(map(int, lines[1].split()))
         matrix = []
-        for i in range(matrix_size[0]):
+        for i in range(matrix_size[1]):
             matrix.append(lines[i+2].split())
-        number_of_sequences = int(lines[2+matrix_size[0]])
+        number_of_sequences = int(lines[2+matrix_size[1]])
         sequences = []
         rewards = []
-        for i in range(3+matrix_size[0], len(lines), 2):
+        for i in range(3+matrix_size[1], len(lines), 2):
             sequences.append((lines[i].replace(" ", "")).rstrip())
             rewards.append(int(lines[i+1]))
     return buffer_size, matrix_size, matrix, number_of_sequences, sequences, rewards
 
 def generate():
-    unique_token_number = int(input("Masukkan jumlah token unik: "))
-    tokens = list(map(str, input("Masukkan token unik: ").split()))
-    buffer_size = int(input("Masukkan ukuran buffer: "))
-    matrix_size = tuple(map(int, input("Masukkan ukuran matriks: ").split()))
-    sequence_number = int(input("Masukkan banyak sekuens: "))
-    max_sequence_size = int(input("Masukkan ukuran maksimum sekuens: "))
+    unique_token_number = int(input("Input number of unique token: "))
+    tokens = list(map(str, input("Input unique token(s): ").split()))
+    buffer_size = int(input("Input buffer size: "))
+    matrix_size = tuple(map(int, input("Input matrix size (col row): ").split()))
+    number_of_sequences = int(input("Input number of sequence: "))
+    max_sequence_size = int(input("Input maximum size of sequence: "))
 
-    matrix = [[None for j in range (matrix_size[1])] for i in range(matrix_size[0])]
-    for i in range(matrix_size[0]):
-        for j in range(matrix_size[1]):
+    matrix = [[None for j in range (matrix_size[0])] for i in range(matrix_size[1])]
+    for i in range(matrix_size[1]):
+        for j in range(matrix_size[0]):
             matrix[i][j] = random.choice(tokens)
     
     rewards = []
     sequences = []
 
-    for i in range(sequence_number):
+    for i in range(number_of_sequences):
         length = random.randint(2, max_sequence_size)
         sequence = ''
         for j in range(length):
@@ -40,40 +42,40 @@ def generate():
         sequences.append(sequence)
         rewards.append(random.randint(0,100))
 
-    return buffer_size, matrix_size, matrix, sequence_number, sequences, rewards
+    return buffer_size, matrix_size, matrix, number_of_sequences, sequences, rewards
 
-def generate_path(current_buffer, index, currentPath, currentCoor, seenPath, buffer_size, paths, matrix_size, matrix, coordinate):
-    if(current_buffer == buffer_size) or (all_sequence(sequences, currentPath) == True):
-        paths.append(currentPath)
-        coordinate.append(currentCoor)
+def generate_path(current_buffer, index, current_path, current_coor, passed, buffer_size, paths, matrix_size, matrix, coordinate):
+    if(current_buffer == buffer_size) or (all_sequence(sequences, current_path) == True):
+        paths.append(current_path)
+        coordinate.append(current_coor)
         return None
     if (current_buffer == 0):
-        for j in range(matrix_size[1]):
-            generate_path(1, j, matrix[0][j], currentCoor+([1,j+1]), [[j, 0]], buffer_size, paths, matrix_size, matrix, coordinate)
+        for j in range(matrix_size[0]):
+            generate_path(1, j, matrix[0][j], current_coor+([j+1, 1]), [[j, 0]], buffer_size, paths, matrix_size, matrix, coordinate)
     elif(current_buffer % 2 == 1):
-        for i in range(matrix_size[0]):
+        for i in range(matrix_size[1]):
             seen = False
-            for coordinates in seenPath:
+            for coordinates in passed:
                 if coordinates[0] == index and coordinates[1] == i:
                     seen = True
                     break
             if (seen == True):
                 continue
-            seenPath.append([index, i])
-            generate_path(current_buffer+1, i, currentPath+matrix[i][index], currentCoor+([index+1, i+1]), seenPath, buffer_size, paths, matrix_size, matrix, coordinate)
-            seenPath.pop()
+            passed.append([index, i])
+            generate_path(current_buffer+1, i, current_path+matrix[i][index], current_coor+([index+1, i+1]), passed, buffer_size, paths, matrix_size, matrix, coordinate)
+            passed.pop()
     else:
-        for j in range(matrix_size[1]):
+        for j in range(matrix_size[0]):
             seen = False
-            for coordinates in seenPath:
+            for coordinates in passed:
                 if coordinates[1] == index and coordinates[0] == j:
                     seen = True
                     break
             if (seen == True):
                 continue
-            seenPath.append([j, index])
-            generate_path(current_buffer+1, j, currentPath+matrix[index][j], currentCoor+([j+1, index+1]), seenPath, buffer_size, paths, matrix_size, matrix, coordinate)
-            seenPath.pop()
+            passed.append([j, index])
+            generate_path(current_buffer+1, j, current_path+matrix[index][j], current_coor+([j+1, index+1]), passed, buffer_size, paths, matrix_size, matrix, coordinate)
+            passed.pop()
     return paths, coordinate
 
 def all_sequence(sequences, path):
@@ -145,27 +147,133 @@ def optimize(path, sequences, initreward, coor):
     else:
         return path, coor
 
-def save_solution(file_name, max_reward, max_sequence, max_buffer, max_coordinates, execution_time):
+def save_solution(file_name, max_reward, max_sequence, max_coordinates, execution_time, choice, matrix, sequences, matrix_size, rewards):
     with open(file_name, 'w') as file:
-        file.write(f'Max reward: {max_reward}\n')
-        file.write(f'Max sequence: {max_sequence}\n')
-        file.write(f'Max buffer: {max_buffer}\n')
-        file.write(f'Max coordinates: {max_coordinates}\n')
-        file.write(f'Execution time: {execution_time} ms\n')
+        if (choice == 2):
+            for i in range(matrix_size[1]):
+                for j in range(matrix_size[0]):
+                    file.write(matrix[i][j]+" ")
+                file.write("\n")
+            file.write("\n")
 
+            for j in range(len(sequences)):
+                sequence = sequences[j]
+                for i in range(len(sequence)):
+                    if i == len(sequence) - 1:
+                        file.write(sequence[i]+'\n')
+                        file.write(str(rewards[j])+'\n')
+                    else:
+                        if i % 2 == 1:
+                            file.write(sequence[i]+' ')
+                        else:
+                            file.write(sequence[i])
+                file.write("\n")
+            file.write("\n")    
+        file.write(str(max_reward))
+        file.write("\n")
+        for i in range(len(max_sequence)):
+            if i == len(max_sequence) - 1:
+                file.write(max_sequence[i])
+            else:
+                if i % 2 == 1:
+                    file.write(max_sequence[i]+' ')
+                else:
+                    file.write(max_sequence[i])
+        file.write("\n")
+        for i in range(len(max_coordinates)):
+            if i % 2 == 0:
+                file.write(str(max_coordinates[i])+', ')
+            else:
+                file.write(str(max_coordinates[i]))
+                file.write("\n")
+        file.write("\n")
+        file.write(execution_time+" ms")
 
-start_time = time.time()
-file_name = input('Enter the file name with the game matrix (or press enter to generate one): ')
-if file_name:
-    buffer_size, matrix_size, matrix, number_of_sequences, sequences, rewards = read_file(file_name)
-else:
-    buffer_size, matrix_size, matrix, number_of_sequences, sequences, rewards = generate()
-    print(matrix)
-    print(sequences)
-paths, coordinate = generate_path(0, 0, [], [], [], buffer_size, [], matrix_size, matrix, [])
-maxreward = find_sequence(sequences, paths)
-max_num, path, coor = (find_optimal(maxreward, paths))
-path, coor = optimize(path, sequences, max_num, coor)
-print(path, coor)
-end_time = time.time()
-print(end_time-start_time, "s")
+print("Cyberpunk 2077 Breach Protocol Solution")
+print("=======================================")
+print("1. Input Text File")
+print("2. Generate Matrix and Sequence")
+print("3. Exit")
+print("=======================================")
+choice = int(input("Choice: "))
+print("=======================================")
+stop_program = False
+while(1 <= choice <= 3 and stop_program == False):
+    if (1 <= choice <= 2):
+        if (choice == 1):
+            file_name = input("Enter the file name with the game matrix: ")
+            relative_path = "test/"+file_name
+            while not os.path.isfile(relative_path):
+                file_name = input("File not found, reenter your file name: ")
+                relative_path = "test/"+file_name
+            buffer_size, matrix_size, matrix, number_of_sequences, sequences, rewards = read_file(relative_path)
+        else:
+            buffer_size, matrix_size, matrix, number_of_sequences, sequences, rewards = generate()
+            print("=======================================")
+            for i in range(matrix_size[1]):
+                for j in range(matrix_size[0]):
+                    if (j == matrix_size[0]-1):
+                        print(matrix[i][j], end='\n')
+                    else:
+                        print(matrix[i][j], end=' ')
+
+            for j in range(len(sequences)):
+                sequence = sequences[j]
+                for i in range(len(sequence)):
+                    if i == len(sequence) - 1:
+                        print(sequence[i])
+                        print(str(rewards[j])+"\n")
+                    else:
+                        if i % 2 == 1:
+                            print(sequence[i], end=' ')
+                        else:
+                            print(sequence[i], end='')   
+
+        start_time = time.time()
+        paths, coordinate = generate_path(0, 0, [], [], [], buffer_size, [], matrix_size, matrix, [])
+        max_reward = find_sequence(sequences, paths)
+        max_num, path, coor = (find_optimal(max_reward, paths))
+        path, coor = optimize(path, sequences, max_num, coor)
+        print("=======================================")
+        print(max_num)
+        for i in range(len(path)):
+            if i == len(path) - 1:
+                print(path[i])
+            else:
+                if i % 2 == 1:
+                    print(path[i]+' ', end='')
+                else:
+                    print(path[i], end='')
+        for i in range(len(coor)):
+            if i % 2 == 0:
+                print(str(coor[i])+', ', end='')
+            else:
+                print(str(coor[i]), end='\n')
+        end_time = time.time()
+        print("\n")
+        print((end_time-start_time)*1000, "ms\n")
+        save_file = input("Do you want to save this solution? (y/n): ")
+        if(save_file == 'y'):
+            file_name_save = input("Input file name: ")
+            relative_path_save = "test/"+file_name_save
+            while os.path.isfile(relative_path_save):
+                file_name_save = input("File name is taken, reenter your file name: ")
+                relative_path_save = "test/"+file_name_save
+            save_solution(relative_path_save, max_num, path, coor, str((end_time-start_time)*1000), choice, matrix, sequences, matrix_size, rewards)
+            print("Your file has been saved!")
+        print("Cyberpunk 2077 Breach Protocol Solution")
+        print("=======================================")
+        print("1. Input Text File")
+        print("2. Generate Matrix and Sequence")
+        print("3. Exit")
+        print("=======================================")
+        choice = int(input("Choice: "))
+        print("=======================================")
+    else:
+        print("=======================================")
+        print("             Thank You!                ")
+        print("=======================================")
+        print("         Shazya Audrea Taufik          ")
+        print("               13522063                ")
+        print("=======================================")
+        stop_program = True
